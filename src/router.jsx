13a97b75a955fcc13516f52e21597a3ac7b07e44
cunde <a18811150085@163.com>
2017-03-28 09:53:23 +0800

    md fixed

--- src/router.jsx
+++ src/router.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 // import { Router, Route, Link, createHashHistory, customHistory } from 'react-router'
 import App from './App';
 import AboutPage from './about.jsx';
+import TabPages from './newtabs/tab.jsx';
 import {Router, Route, Switch} from 'react-router'
 import createBrowserHistory from 'history/createBrowserHistory'
 
@@ -12,6 +13,7 @@ export default(
         <Switch>
             <Route exact={true} path="/" component={App}/>
             <Route path="/about" component={AboutPage}/>
+            <Route pathe="/us" component={TabPages} />
         </Switch>
     </Router>
 )
